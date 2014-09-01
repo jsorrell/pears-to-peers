ba6b6bcd5503c9497b1c85437f34aca27065dc59
Jack Sorrell <jsorrell@andrew.cmu.edu>
2014-09-01 01:48:35 -0400

    fix errors

--- streamingserver.js
+++ streamingserver.js
@@ -7,19 +7,19 @@ http.createServer(uploadHandler).listen(8081);
 
 function uploadHandler(request,response){
     response.writeHead(200,{'Access-Control-Allow-Origin': '*'});
-    var destinationFile = fs.createWriteStream(filename.toString());      
+    var destinationFile = fs.createWriteStream(filename.toString() + ".m4v");      
     filename += 1;
     request.pipe(destinationFile);
 
     var fileSize = request.headers['content-length'];
     var uploadedBytes = 0;
 
-    request.on('data',function(d){  
+    request.on('data',function(d){
         console.log("got data");              
         uploadedBytes += d.length;
         var p = (uploadedBytes/fileSize) * 100;
         response.write("Uploading " + parseInt(p)+ " %\n");
-        console.log("Uploading " + parseInt(p)+ " %\n");
+        console.log("Uploading " + parseInt(p));
     });
 
     request.on('end', function(){
