5117c817ce34a213bf4b4d5b0956664a5496120e
Jack Sorrell <jsorrell@andrew.cmu.edu>
2014-09-01 01:24:09 -0400

    allow local uploads

--- streamingserver.js
+++ streamingserver.js
@@ -6,17 +6,19 @@ console.log("STARTING STREAMING SERVER ON PORT 8081");
 http.createServer(uploadHandler).listen(8081);
 
 function uploadHandler(request,response){
-    response.writeHead(200);
+    response.writeHead(200,{'Access-Control-Allow-Origin': '*'});
     var destinationFile = fs.createWriteStream(filename.toString());      
     request.pipe(destinationFile);
 
     var fileSize = request.headers['content-length'];
     var uploadedBytes = 0 ;
 
-    request.on('data',function(d){                
+    request.on('data',function(d){  
+        console.log("got data");              
         uploadedBytes += d.length;
         var p = (uploadedBytes/fileSize) * 100;
         response.write("Uploading " + parseInt(p)+ " %\n");
+        console.log("Uploading " + parseInt(p)+ " %\n");
     });
 
     request.on('end', function(){
