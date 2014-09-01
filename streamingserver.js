3496ad2592feeb54695525842b55faf81d58cc5e
Carl <wumu727@gmail.com>
2014-09-01 04:21:36 -0400

    added express multipart instead of bodyparser

--- streamingserver.js
+++ streamingserver.js
@@ -1,26 +1,19 @@
 var fs = require ('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
+var multiparty = require('connect-multiparty');
 
 console.log("STARTING STREAMING SERVER ON PORT 8081");
 var app = express();
 app.listen(8081);
 
 app.use(bodyParser({uploadDir:'./uploads'})); //TODO: deprecated
-
+app.use(multiparty());
 app.post('/file-upload', uploadHandler);
 
 function uploadHandler(req,res){
     console.log(req.body);
     console.log(req.files);
-    
-    req.form.on('progress', function(bytesReceived, bytesExpected) {
-        console.log(((bytesReceived / bytesExpected)*100) + "% uploaded");
-    });
-    
-    req.form.on('end', function(){
-        res.end("File Upload Complete");
-    });
     
     var tmp_path = req.files.upload.path;
     // set where the file should actually exists - in this case it is in the "images" directory
